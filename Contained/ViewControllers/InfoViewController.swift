6441fb0132e55c7f7523f499acca51051d2de4c2
Jake Connerly <jlconnerly@gmail.com>
2019-07-22 15:22:20 -0500

    Info Screen Complete

---
+++ Contained/ViewControllers/InfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  InfoViewController.swift
+//  Contained
+//
+//  Created by Jake Connerly on 7/22/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func done(_ sender: UIBarButtonItem) {
+        navigationController?.popToRootViewController(animated: true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
