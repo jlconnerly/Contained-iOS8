ee4b6dbace22eecbaec6265d3659a89f60f47c92
Jake Connerly <jlconnerly@gmail.com>
2019-07-22 14:42:11 -0500

    Boiler plate set up complete

---
+++ Contained/Models/Settings.swift
@@ -0,0 +1,17 @@
+//
+//  Settings.swift
+//  Contained
+//
+//  Created by Jake Connerly on 7/22/19.
+//  Copyright © 2019 jake connerly. All rights reserved.
+//
+
+import Foundation
+
+struct Settings {
+    static let shared = Settings()
+    private init() {}
+    
+    var shouldRoll = false
+    var shouldZoom = false
+}
