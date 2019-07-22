b7b53b50aaf8b6a48efb0b700102815126625f50
Jake Connerly <jlconnerly@gmail.com>
2019-07-22 15:45:32 -0500

    Game Screen Complete. MVP Complete

--- Contained/ViewControllers/GameViewController.swift
+++ Contained/ViewControllers/GameViewController.swift
@@ -7,16 +7,28 @@
 //
 
 import UIKit
+import SpriteKit
 
 class GameViewController: UIViewController {
 
+    @IBOutlet weak var skView: SKView!
+    
+    var skScene: CustomScene? = nil
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        skScene = CustomScene(size: view.bounds.size)
+        skView.presentScene(skScene)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+ 
+    
     /*
     // MARK: - Navigation
 
